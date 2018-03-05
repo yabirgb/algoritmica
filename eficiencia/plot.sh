9475bf3ae989be1f8ed2c15dc7da671b9e6aff79
Yabir <yabirgb@gmail.com>
2018-03-05 20:40:30 +0100

    Added new plots

--- eficiencia/plot.sh
+++ eficiencia/plot.sh
@@ -1,19 +1,21 @@
 #/bin/bash
 
-files=(burbuja insercion seleccion mergesort quicksort heapsort)
+files=(burbuja insercion seleccion mergesort quicksort heapsort hanoi floyd)
 cuadraticos=(burbuja insercion seleccion)
 logaritmos=(mergesort quicksort heapsort)
+cubicos=(floyd)
+exponencial=(hanoi)
 plots="plots"
 data="output"
 level=$1
-comparative=$2
+
+ordenacion=("${cuadraticos[@]}" "${logaritmos[@]}")
 
 
 #Graficas compartidas
 
-if [ $2 == "-c" ];
-then
-  
+echo "rm $plots/*"
+
 gnuplot <<- EOF
         set xlabel "Tamaño del vector"
         set ylabel "Tiempo (seg)"
@@ -31,8 +33,45 @@ gnuplot <<- EOF
 	plot "${data}/burbuja_O${level}.dat", "${data}/seleccion_O${level}.dat", "${data}/insercion_O${level}.dat" , burbuja(x), seleccion(x), inserccion(x)
 EOF
 
-else
-    
+#===============================
+
+muchos=""
+for file in "${logaritmos[@]}"
+do
+
+    nuevo="'${data}/${file}_O${level}.dat' with lines, "
+    muchos=$muchos$nuevo
+done
+
+gnuplot <<- EOF
+        set xlabel "Tamaño del vector"
+        set ylabel "Tiempo (seg)"
+        set title "Comparativa algoritmos de ordenación nlogn -O${level}"   
+        set term png
+        set output "${plots}/logaritmicos_O${level}.png"
+        plot ${muchos::-2}
+EOF
+
+#===============================
+muchos=""
+for file in "${cuadraticos[@]}"
+do
+
+    nuevo="'${data}/${file}_O${level}.dat' with lines, "
+    muchos=$muchos$nuevo
+done
+
+gnuplot <<- EOF
+        set xlabel "Tamaño del vector"
+        set ylabel "Tiempo (seg)"
+        set title "Comparativa algoritmos de ordenación n^2 -O${level}"   
+        set term png
+        set output "${plots}/cuadraticos_O${level}.png"
+        plot ${muchos::-2}
+EOF
+
+#===============================
+
 #Plot data with dots
 for file in "${files[@]}"
 do
@@ -89,11 +128,43 @@ gnuplot <<- EOF
         set ylabel "Tiempo (seg)"
         set title "Eficiencia híbrida ${file} -O${1}"   
         set term png
-        set output "${plots}/${file}_O${level}_O${1}_fit.png"
-	set fit logfile 'fit_data/${file}_${1}_fit.log'
+        set output "${plots}/${file}_O${1}_fit.png"
+	set fit logfile 'fit_data/${file}_O${1}_fit.log'
 	f(x) = a*log(x+b) + c
         fit f(x) "${data}/${file}_O${level}.dat" via a,b,c
 	plot "${data}/${file}_O${level}.dat", f(x)
 EOF
 done
-fi
+
+for file in "${cubicos[@]}"
+do
+    
+gnuplot <<- EOF
+        set xlabel "Tamaño del vector"
+        set ylabel "Tiempo (seg)"
+        set title "Eficiencia híbrida ${file} -O${level}"   
+        set term png
+        set output "${plots}/${file}_O${level}_fit.png"
+	set fit logfile 'fit_data/${file}_O${level}_fit.log'
+	f(x) = a*x**3 + b*x**2 + c*x + d
+        fit f(x) "${data}/${file}_O${level}.dat" via a,b,c,d
+	plot "${data}/${file}_O${level}.dat", f(x)
+EOF
+done
+
+
+for file in "${exponencial[@]}"
+do
+    
+gnuplot <<- EOF
+        set xlabel "Tamaño del vector"
+        set ylabel "Tiempo (seg)"
+        set title "Eficiencia híbrida ${file} -O${level}"   
+        set term png
+        set output "${plots}/${file}_O${level}_fit.png"
+	set fit logfile 'fit_data/${file}_O${level}_fit.log'
+	f(x) = a*2**(x+c) + b
+        fit f(x) "${data}/${file}_O${level}.dat" via a,b,c
+	plot "${data}/${file}_O${level}.dat", f(x)
+EOF
+done
